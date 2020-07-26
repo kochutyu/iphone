2ddae5cdfa19183b10e518f1f3fcc261acbdc28f
Yura Kochut <kochutyurawork@gmail.com>
2020-07-26 19:30:12 +0300

    add setColor (core)

--- core/color.js
+++ core/color.js
@@ -1,5 +1,5 @@
-function toRGB(hex) {
-    if (!hex) return console.error('toRGB() method expects one argument.');
+function HEXtoRGB(hex) {
+    if (!hex) return console.error('HEXtoRGB() method expects one argument.');
 
     const hexValue = hex
         .split('')
@@ -8,11 +8,12 @@ function toRGB(hex) {
 
     const long = hexValue.length === 6;
 
+
     if (long) {
         const color = {
-            r: parseInt(hexValue[1] + hexValue[2], 16),
-            g: parseInt(hexValue[3] + hexValue[4], 16),
-            b: parseInt(hexValue[5] + hexValue[6], 16)
+            r: parseInt(hexValue[0] + hexValue[1], 16),
+            g: parseInt(hexValue[2] + hexValue[3], 16),
+            b: parseInt(hexValue[4] + hexValue[5], 16)
         }
         return `rgb(${color.r}, ${color.g}, ${color.b})`;
     }
@@ -30,11 +31,11 @@ function toRGB(hex) {
         return `rgb(${color.r}, ${color.g}, ${color.b})`;
     }
 
-    return console.error(`"${hex}" is invalid. Please enter in format "#1c1c1c" !`);
+    return console.error(`"${hex}" is invalid. Please enter in format "#1c1c1c" or "#111" !`);
 }
 
-function toHEX(rgb) {
-    if (!rgb) return console.error('toHEX() method expects one argument.');
+function RGBtoHEX(rgb) {
+    if (!rgb) return console.error('RGBtoHEX() method expects one argument.');
 
     const condition = ['r', 'g', 'b', 'a', '(', ')', ' '];
 
@@ -52,7 +53,7 @@ function toHEX(rgb) {
         const convertRBGtoHEX = rgbValues.map(symbol => {
             let number = +symbol;
             const hex = number.toString(16);
-            return hex.length == 1 ? "0" + hex : hex;
+            return hex.length === 1 ? "0" + hex : hex;
         });
         return `#${convertRBGtoHEX[0]}${convertRBGtoHEX[1]}${convertRBGtoHEX[2]}`;
     }
@@ -68,4 +69,49 @@ function toHEX(rgb) {
     }
 
     return console.error(`"${rgb}" is invalid. Please enter in format "rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})" !`);
+}
+
+function RGBtoRGB(rgb) {
+    if (!rgb) return console.error('RBGtoRGB() method expects one argument.');
+
+    const condition = ['r', 'g', 'b', 'a', '(', ')', ' '];
+
+    const rgbValues = rgb
+        .split('')
+        .filter(symbol => !condition.some(sym => sym === symbol))
+        .join('')
+        .split(',')
+        .filter(symbol => symbol.split('').length < 4 && symbol.split('').length > 0)
+        .map(symbol => +symbol)
+
+    const rgbValid = rgbValues.every(num => num >= 0 && num <= 255);
+
+    if (rgbValues.length === 3 && rgbValid) {
+        const rgbValue = rgbValues.join(', ');
+        return `rgb(${rgbValue})`;
+    }
+
+    if (!rgbValid) {
+        const invalidNumber = rgbValues.map(num => {
+            if (num >= 0 && num <= 255) {
+                return num;
+            }
+            return 'invalid';
+        });
+        return console.error(`"${rgb}" is invalid. Please enter in format "rgb(${invalidNumber[0]}, ${invalidNumber[1]}, ${invalidNumber[2]})" !`);
+    }
+
+    return console.error(`"${rgb}" is invalid. Please enter in format "rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})" !`);
+
+}
+
+function setColor(color) {
+    const type = ['rgb', 'rgba', '#'];
+    const result = type.find(type => color.includes(type));
+
+    const RGB = 'rgb' || 'rgba';
+    const HEX = '#';
+
+    if (result === HEX) return HEXtoRGB(color);
+    if (result === RGB) return RGBtoHEX(color);
 }
